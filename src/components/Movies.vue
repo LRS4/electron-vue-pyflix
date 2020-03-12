17684fe09af283939fcfbd9ca0c1ca28ac949c38
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-03-12 17:50:19 +0000

    Implemented local storage solution methods

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <button v-on:click="addNewItem">Add new item</button>
     <div v-if="!moviesData" class="loading">Loading Please wait...</div>
     <div v-else v-for="movie in moviesData" v-bind:key="movie.imdbId" class="movies">
      <h3>{{ movie.Title }}</h3>
@@ -18,6 +19,8 @@ Fetch has a few shortcomings. Like we demonstrated earlier with fetch, we needed
 This is simplified with axios. Let’s replace our current fetch function with axios.
 */
 import axios from 'axios';
+import moment from 'moment';
+const storage = require('electron-storage');
 
 export default {
   name: 'Movies',
@@ -27,22 +30,90 @@ export default {
     };
   },
   methods: {
-    populateMovieData() {
-      const movieNames = ['Gladiator', 'The Matrix', 'The Terminator']; // the values for the API lookup
-      
-      // https://stackoverflow.com/questions/56532652/axios-get-then-in-a-for-loop
-      let movies = [];
-      let promises = [];
-      movieNames.forEach(name => {
-        promises.push(
-          axios.get(`http://www.omdbapi.com/?t=${ name }&y=&apikey=ff0c3dab`).then(response => {
-            movies.push(response.data);
-          })
-        )
-      })
+    addNewItem() {
+      storage.get('movies')
+      .then(data => {
 
-      Promise.all(promises).then(() => this.moviesData = movies);
-      console.log(movies);
+        // create new item to add to model
+        axios.get('http://www.omdbapi.com/?&t=we were soldiers&apikey=ff0c3dab')
+        .then(response => {
+          let newData = response.data;
+
+          // check item does not already exist
+          for (let movie in data) {
+            if (data[movie].Title == newData.Title && data[movie].imdbID == newData.imdbID) {
+              return console.error(`Duplicate error! The movie '${newData.Title}' is already in the database.`);
+            }
+          }
+
+          // proceed if not a duplicate
+          // add additional properties to the new item
+          newData.watchCount = 0;
+          newData.dateLastWatched = 'Not watched';
+          newData.minuteLastWatched = 0;
+          newData.myRating = 0;
+          newData.fileLocation = 'C://somepath';
+          newData.dateAdded = moment();
+          data.push(newData);
+          console.log(newData);
+
+          // save to cached file
+          storage.set('movies', data)
+          .then(() => {
+            console.log(`A new movie '${response.data.Title}' was added to the cached data.`);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+        });
+
+        // update view model
+        this.moviesData = data;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    },
+    populateMovieData() {
+      console.log('Attempting to retrieve movies.json from local storage...');
+      storage.isPathExists('movies.json', (itDoes) => {
+        if (itDoes) {
+          storage.get('movies')
+          .then(data => {
+            console.log(`${data.length} items retrieved successfully.`);
+            this.moviesData = data;
+          })
+          .catch(err => {
+            console.error(err);
+          });
+        } else {
+          const movieNames = ['Gladiator', 'The Matrix', 'The Terminator']; // the values for the API lookup
+      
+          // https://stackoverflow.com/questions/56532652/axios-get-then-in-a-for-loop
+          let movies = [];
+          let promises = [];
+          console.log('Calling OMDB API...');
+          movieNames.forEach(name => {
+            promises.push(
+              axios.get(`http://www.omdbapi.com/?t=${ name }&y=&apikey=ff0c3dab`).then(response => {
+                movies.push(response.data);
+              })
+            )
+          })
+          Promise.all(promises).then(() => {
+            console.log('Saving response to local storage...')
+            storage.set('movies', movies)
+            .then(() => {
+              console.log('The file was successfully written to local storage.'); // C:\Users\L.Spencer\AppData\Roaming\electron-vue\movies.json
+              console.log(`${promises.length} items were saved successfully.`)
+            })
+            .catch(err => {
+              console.error(err);
+            });
+            this.moviesData = movies;
+          });
+        }
+      })
     }
   },
   created() {
