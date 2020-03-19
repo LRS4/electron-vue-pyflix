a45a8f9b8d222ac328aa39ff667f2ca4b34fb7ba
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-03-19 18:56:50 +0000

    Added Vuex store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { store } from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -11,5 +12,6 @@ Vue.use(IconsPlugin)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,115 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+import moment from 'moment';
+const chunk = require('chunk');
+const storage = require('electron-storage');
+const path = require('path');
+const fs = require('fs');
+
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    state: { // data
+        movies : [],
+        filter: ""
+    },
+    actions: { // methods
+        loadMovies({ commit }) {
+            console.log('Attempting to retrieve movies.json from local storage...');
+            storage.isPathExists('movies.json', (itDoes) => {
+                if (itDoes) {
+                storage.get('movies')
+                .then(data => {
+                    console.log(`${data.length} items retrieved successfully.`);
+                    let movies = data.sort((a, b) => a.Title.localeCompare(b.Title)); // sort alphabetical
+                    commit('SET_MOVIES', movies);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+                } else {
+                // the values for the API lookup from HDD
+                const movieItemsHDD = JSON.parse(fs.readFileSync(path.normalize('C:\\Users\\L.Spencer\\Documents\\GitHub\\electron-vue-pyflix\\utils\\hdd_data.json')));
+            
+                // https://stackoverflow.com/questions/56532652/axios-get-then-in-a-for-loop
+                let movies = [];
+                let promises = [];
+                console.log('Calling OMDB API...');
+        
+                movieItemsHDD.forEach(item => {
+                    if (item.Type == 'movie') {
+                    promises.push(
+                        axios.get(`http://www.omdbapi.com/?t=${ item.Title }&y=${ item.Year }&apikey=ff0c3dab`)
+                        .then(response => {
+                        if (response.data.Error == 'Movie not found!') { 
+                            return console.error(`Undefined error! The movie ${item.Title} could not be found.`); 
+                        }
+                        
+                        let newData = response.data;
+                        newData.watchCount = 0;
+                        newData.dateLastWatched = 'Not watched';
+                        newData.minuteLastWatched = 0;
+                        newData.myRating = 0;
+                        newData.fileLocation = item.FileLocation;
+                        newData.dateAdded = moment();
+                        console.log(newData);
+                        movies.push(newData);
+                        })
+                    )
+                    } else {
+                    promises.push(
+                        axios.get(`http://www.omdbapi.com/?t=${ item.Title }&apikey=ff0c3dab`)
+                        .then(response => {
+                        let newData = response.data;
+                        newData.watchCount = 0;
+                        newData.dateLastWatched = 'Not watched';
+                        newData.minuteLastWatched = 0;
+                        newData.myRating = 0;
+                        newData.fileLocation = 'C://somepath';
+                        newData.dateAdded = moment();
+                        movies.push(newData);
+                        })
+                    )
+                    }
+                })
+                
+                Promise.all(promises).then(() => {
+                    console.log('Saving response to local storage...')
+                    storage.set('movies', movies)
+                    .then(() => {
+                    console.log('The file was successfully written to local storage.'); // C:\Users\L.Spencer\AppData\Roaming\electron-vue\movies.json
+                    console.log(`${promises.length} items were saved successfully.`)
+                    })
+                    .catch(err => {
+                    console.error(err);
+                    });
+                    commit('SET_MOVIES', movies);
+                });
+                }
+            })
+        },   
+        setFilter({ commit }, value) {
+            console.log(value)
+            commit('SET_FILTER', value);
+        }
+    },
+    mutations: { // setters
+        SET_MOVIES (state, movies) {
+            state.movies = movies
+        },
+        SET_FILTER(state, value) {
+            state.filter = value
+        }
+    },
+    getters: { // computed
+        getMovies(state) {
+            if (state.filter == undefined || state.filter == "") {
+                return chunk(state.movies, 6);
+            } else {
+                return chunk(state.movies.filter(item => item.Genre.includes(state.filter)), 6);
+            }
+        }
+    }
+})
