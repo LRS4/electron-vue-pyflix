c534e85f6034565bc4330041802ac1522b8b2468
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-03-19 23:42:48 +0000

    Added Menu component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,6 @@
 <template>
   <div id="app">
-    <!-- BootstrapVue supports 'to=' attribute from Vue Router -->
-    <!-- previously <router-link to='/'>Home</router-link> -->
-    <div>
-      <b-navbar toggleable="lg" type="dark" variant="dark">
-        <b-navbar-brand to="/" href="#">Piflix</b-navbar-brand>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item to="/about">About</b-nav-item>
-            <b-nav-item to="/movie/the terminator">Series</b-nav-item>
-            <b-nav-item href="https://www.imdb.com" target="_blank">Action</b-nav-item>
-            <b-nav-item href="#">Drama</b-nav-item>
-            <b-nav-item href="#">Horror</b-nav-item>
-            <b-nav-item href="#">Sci-Fi</b-nav-item>
-            <b-nav-item href="#">Comedy</b-nav-item>
-          </b-navbar-nav>  
-
-          <b-navbar-nav>
-            <b-nav-item-dropdown left>
-              <!-- Using 'button-content' slot -->
-              <template v-slot:button-content>
-                <b>Filters</b>
-              </template>
-              <b-dropdown-item href="#">Top Rated</b-dropdown-item>
-              <b-dropdown-item href="#">Most Watched</b-dropdown-item>
-              <b-dropdown-item href="#">Recently Added</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
-
+    <Menu />
     <!-- route outlet -->
     <!-- component matched by the route will render here -->
     <transition name="view">
@@ -51,10 +10,14 @@
 </template>
 
 <script>
+import Menu from './components/Menu';
 import { mapState } from 'vuex'
 
 export default {
   name: 'App',
+  components: {
+    Menu
+  },
   mounted() {
     this.$store.dispatch('loadMovies');
   },
---
+++ src/components/Menu.vue
@@ -0,0 +1,60 @@
+<template>
+    <!-- BootstrapVue supports 'to=' attribute from Vue Router -->
+    <!-- previously <router-link to='/'>Home</router-link> -->
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand to="/" v-on:click='filterItems("")'>Piflix</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("")'>All</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Action")'>Action</b-nav-item>            
+            <b-nav-item to="/" v-on:click='filterItems("Adventure")'>Adventure</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Animation")'>Animation</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Biography")'>Biography</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Comedy")'>Comedy</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Crime")'>Crime</b-nav-item>            
+            <b-nav-item to="/" v-on:click='filterItems("Drama")'>Drama</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Horror")'>Horror</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Sci-Fi")'>Sci-Fi</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("Thriller")'>Thriller</b-nav-item>
+            <b-nav-item to="/" v-on:click='filterItems("War")'>War</b-nav-item>
+          </b-navbar-nav>  
+
+          <b-navbar-nav>
+            <b-nav-item-dropdown left>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <b>Filters</b>
+              </template>
+              <b-dropdown-item href="#">Top Rated</b-dropdown-item>
+              <b-dropdown-item href="#">Most Watched</b-dropdown-item>
+              <b-dropdown-item href="#">Recently Added</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  methods: {
+    filterItems(filter) {
+        this.$store.dispatch('setFilter', filter)
+    }
+  }
+}
+</script>
