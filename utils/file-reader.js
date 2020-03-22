bcb9333fc8df7b5c42f89a3968b9dfa3046793a2
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-03-22 17:19:07 +0000

    Updated file read unit test

--- utils/file-reader.js
+++ utils/file-reader.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-let hdd_data = JSON.parse(fs.readFileSync(path.join(__dirname, 'hdd_data.json')));
+let movieItemsHDD = JSON.parse(fs.readFileSync(path.join(__dirname, 'hdd_data.json')));
+var seasons = [];
 
-hdd_data.forEach(item => {
-    (item.Type == 'movie') ? console.log(`Movie: ${item.Title} Year: ${item.Year}`) : console.log(`Series: ${item.Title}`)
+movieItemsHDD.forEach(item => {
+    // (item.Type == 'movie') ? console.log(`Movie: ${item.Title} Year: ${item.Year}`) : console.log(`Series: ${item.Title}`)
+    if (item.Type == 'series') { 
+        for (let key in item) {
+            if (key.includes('Season')) {
+                console.log(item[key]);
+                seasons.push(item[key]);
+            }
+        }
+    }
 });
+
+console.log(seasons);
