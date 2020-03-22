170c96519f6ca9c1914588d598103eb46fc36e5f
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-03-22 20:14:02 +0000

    Added config changes

--- src/background.js
+++ src/background.js
@@ -17,8 +17,6 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ 
-    width: 800,
-    height: 600,
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false
